3f72fdb5d3137c62db0d0a4c04bc9e96436075d1
unknown <vdabas@cc-cwhite1.ic.ac.uk>
2015-08-03 17:01:59 +0100

    Pass two numbers test

--- src/main/java/org/arm/service/CalculatorService.java
+++ src/main/java/org/arm/service/CalculatorService.java
@@ -9,7 +9,7 @@ public class CalculatorService {
 	public int add(String string) {
 		
 		if(StringUtils.isNotBlank(string)) {
-			String[] numbersArray = StringUtils.split(",") ;
+			String[] numbersArray = string.split(",") ;
 			return Arrays.asList(numbersArray).parallelStream().map(i -> Integer.parseInt(i)).mapToInt(Integer::valueOf).sum();
 		}
 		return 0;
