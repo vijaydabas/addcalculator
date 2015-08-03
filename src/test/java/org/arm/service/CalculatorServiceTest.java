ba0f22cc4cd7e4bdb87b806caefdbd565520853c
unknown <vdabas@cc-cwhite1.ic.ac.uk>
2015-08-03 16:18:12 +0100

    Initial Commit

---
+++ src/test/java/org/arm/service/CalculatorServiceTest.java
@@ -0,0 +1,20 @@
+package org.arm.service;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class CalculatorServiceTest {
+
+	
+	@Test
+	public void whenEmptyStringThenReturnZero() throws Exception {
+		CalculatorService calculator = new CalculatorService();
+		assertTrue(0 == calculator.add(""));
+		
+	}
+	
+}
